e4664da9ea5feb620d4de4b16b1db6e75a2a2ee2
SteakingCoder <1941499609@qq.com>
2018-03-18 20:08:39 +0800

    修复无端文字出现中划线的bug

--- src/main/java/com/controller/SttafOperationController.java
+++ src/main/java/com/controller/SttafOperationController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -42,4 +44,7 @@ public class SttafOperationController {
         result.put("status", "success");
         return result;
     }
+
+
+
 }
