f68142324eba00ff5172368d5f88826a58da01f1
SteakingCoder <1941499609@qq.com>
2018-03-31 21:22:21 +0800

    从main_staff的前后端逻辑中产生main_custm,针对客户的处理

---
+++ src/main/java/com/controller/CustmOperationController.java
@@ -0,0 +1,179 @@
+package com.controller;
+
+import com.bean.CreditCard;
+import com.bean.StaffOperRecord;
+import com.service.CreditAccountService;
+import com.service.CustomerService;
+import com.service.StaffOperationService;
+import com.service.StaffService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by HuangDanGeeker on 2018/3/18.
+ * controller : dealing with staff operation request
+ */
+@Controller
+@RequestMapping("/custm")
+public class CustmOperationController {
+
+    @Resource
+    private StaffOperationService staffOperationService;
+    @Resource
+    private CreditAccountService creditCardService;
+    @Resource
+    private CustomerService customerService;
+
+    @RequestMapping("/createcreditcard/{custmNo}/{cardNo}")
+    @ResponseBody
+    public Map<String,String> createcreditcard(@CookieValue("userNo") String userNo, @PathVariable String custmNo, @PathVariable String cardNo){
+        System.out.println("createcreditcard");
+        System.out.println("cutmNo " + custmNo);
+        System.out.println("creditCardNum " + cardNo);
+
+        creditCardService.createCreditCard(custmNo, cardNo);
+
+        Map result = new HashMap<String, String>();
+        result.put("status", "success");
+        //Staff operation record
+        staffOperationService.insertRecord(userNo, "CreateCreditCard", custmNo, customerService.queryCustomerName(custmNo), cardNo, null);
+
+        return result;
+    }
+
+    @RequestMapping("/queryCustmNo")
+    @ResponseBody
+    public String queryCustmNo(){
+        System.out.println("queryCustmNo");
+        return String.valueOf(customerService.generateNo());
+
+    }
+    @RequestMapping("/generateCreditCardNo")
+    @ResponseBody
+    public String generateCreditCardNo(){
+        System.out.println("generateCreditCardNo");
+        return creditCardService.generateCreditCardNo();
+    }
+
+    @RequestMapping("/deposit/{custmNo}/{creditCardNum}/{nums}/{type}/{dueTime}")
+    @ResponseBody
+    public Map<String,String> deposit(@CookieValue("userNo") String userNo, @PathVariable String custmNo, @PathVariable String creditCardNum, @PathVariable Integer nums, @PathVariable Integer type,@PathVariable String dueTime){
+        System.out.println("deposit");
+        System.out.println("cutmNo " + custmNo);
+        System.out.println("creditCardNum " + creditCardNum);
+        System.out.println("nums " + nums);
+        System.out.println("type " + type);
+        System.out.println("dueTime " + dueTime);
+        Map result = new HashMap<String, String>();
+
+        //活期&&定期存储的到期时间处理
+        if(type == 1){
+            if(!cheackDueTime(LocalDate.parse(dueTime))){
+                result.put("status", "failure");
+                result.put("reason", "invaild Time : dueTime is prior to today");
+                return result;
+            }
+            creditCardService.deposit(custmNo, creditCardNum, nums, dueTime);
+        }else{
+            int monthScape = 0;
+            int yearScape = 0;
+            switch(dueTime){
+                case "1":monthScape=3;break;
+                case "2":monthScape=6;break;
+                case "3":yearScape=1;break;
+                case "4":yearScape=2;break;
+                case "5":yearScape=3;break;
+            }
+            LocalDate date = LocalDate.now();
+            int month = date.getMonth().getValue();
+            month = month + monthScape;
+            int year = yearScape + date.getYear() + month/12;
+            LocalDate endDate = LocalDate.of(year, (month % 12 + 1), date.getDayOfMonth());
+            System.out.println(endDate.toString());
+            if(!cheackDueTime(endDate)){
+                result.put("status", "failure");
+                result.put("reason", "invaild Time : dueTime is prior to today");
+                return result;
+            }
+            creditCardService.deposit(custmNo, creditCardNum, nums, endDate.toString());
+        }
+
+
+        result.put("status", "success");
+        //Staff operation record
+        staffOperationService.insertRecord(userNo, "deposit", custmNo, customerService.queryCustomerName(custmNo), creditCardNum, nums);
+        return result;
+    }
+    @RequestMapping("/require/{custmNo}/{creditCardNum}/{nums}")
+    @ResponseBody
+    public Map<String,String> require(@PathVariable String custmNo, @PathVariable String creditCardNum, @PathVariable Integer nums){
+        System.out.println("require");
+        System.out.println("cutmNo " + custmNo);
+        System.out.println("creditCardNum " + creditCardNum);
+        System.out.println("nums " + nums);
+        Map result = new HashMap<String, String>();
+
+        //检查是否到期
+        String dueTime = creditCardService.queryDueTime(custmNo, creditCardNum);
+        LocalDate dueDate = LocalDate.parse(dueTime);
+        if(!cheackDueTime(LocalDate.now(), dueDate)){
+            result.put("status", "failure");
+            result.put("reason", "invaild Time : dueTime is prior to today");
+            return result;
+        }
+        creditCardService.require(custmNo, creditCardNum, nums);
+
+        result.put("status", "success");
+        return result;
+    }
+
+
+    private boolean cheackDueTime(LocalDate dueTime){
+        LocalDate now = LocalDate.now();
+        if(dueTime.isBefore(now)){
+            return false;
+        }
+        return true;
+    }
+    private boolean cheackDueTime(LocalDate requireTime, LocalDate dueTime){
+        LocalDate now = LocalDate.now();
+        if(requireTime.isAfter(dueTime)){
+            return true;
+        }
+        return false;
+    }
+
+
+    @RequestMapping("/queryCustmIntrest/{custmNo}")
+    @ResponseBody
+    public Map<String, Object> queryCustmIntrest(@PathVariable("custmNo") String custmNo){
+        System.out.println("queryCustmIntrest");
+        System.out.println("custmNo " + custmNo);
+        Map<String, Object> result = new HashMap<>(5,5);
+        //检查用户账号存在情况
+        if(!customerService.checkCustomer(custmNo)){
+            result.put("status", "error");
+            result.put("reason", "the custmNo is invalid");
+            return result;
+        }
+
+        List<CreditCard> list = creditCardService.queryCreditCardRecord(custmNo);
+        result.put("status", "success");
+        result.put("list", list);
+        result.put("count", list.size());
+        return result;
+
+    }
+
+
+
+}
